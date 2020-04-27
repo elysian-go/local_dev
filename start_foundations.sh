32274a248767359dcfb26b83aaf3138c3359209a
debray_v <victor.debray@epitech.eu>
2020-04-27 20:42:26 +0200

    redis sentinel setup in docker-compose

--- start_foundations.sh
+++ start_foundations.sh
@@ -3,5 +3,4 @@ docker-compose \
 -f docker-compose.yml \
 -f elysian_project/docker-compose.yml \
 -f elysian_account/docker-compose.yml \
-up --remove-orphans redis project-db project-rdb
-
+up --remove-orphans master slave sentinel account-db project-db project-rdb
